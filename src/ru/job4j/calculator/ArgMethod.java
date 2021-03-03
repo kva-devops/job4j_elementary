a8ce0bc69ab21bbd929f39ec002bb83ab2e94a46
kva.devops <kva.devops@gmail.com>
2021-03-03 14:09:18 +0400

    4.1. Аргументы [#484888]

--- src/ru/job4j/calculator/ArgMethod.java
+++ src/ru/job4j/calculator/ArgMethod.java
@@ -5,11 +5,13 @@ public class ArgMethod {
     public static void hello(String name) {
         System.out.println("Hello, " + name);
     }
+
     public static void my_age(int age) {
         System.out.println("My age is " + age);
     }
+
     public static void main(String[] args) {
-        String name = "Vladimir";
+        String name = "Vladimir Kutyavin";
         int age = 33;
         ArgMethod.hello(name);
         ArgMethod.my_age(age);
