c42cb8b3d25c48305bdab4c52ecbda34ca1aa752
kva.devops <kva.devops@gmail.com>
2021-03-04 15:06:29 +0400

    9.2. Checkstyle [#484893] 4

--- src/ru/job4j/calculator/Fit.java
+++ src/ru/job4j/calculator/Fit.java
@@ -6,15 +6,17 @@ public class Fit {
         return (height - 100) * 1.15;
     }
     // method for calculate optimal weight of woman
+
     public static double womanWeight(short height) {
         return (height - 110) * 1.15;
     }
     // start program
+
     public static void main(String[] args) {
-        short your_height = 177;
-        double man = Fit.manWeight(your_height);
-        System.out.println("Man " + your_height + " is " + man + " kg");
-        double woman = Fit.womanWeight(your_height);
-        System.out.println("Woman " + your_height + " is " + woman + " kg");
+        short yourHeight = 177;
+        double man = Fit.manWeight(yourHeight);
+        System.out.println("Man " + yourHeight + " is " + man + " kg");
+        double woman = Fit.womanWeight(yourHeight);
+        System.out.println("Woman " + yourHeight + " is " + woman + " kg");
     }
 }
