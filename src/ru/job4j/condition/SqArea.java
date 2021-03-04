7fbe153ed401b496e1c87709daaeaea4894bcd07
kva.devops <kva.devops@gmail.com>
2021-03-04 14:20:45 +0400

    8. Стороны прямоугольника. [#484880]

---
+++ src/ru/job4j/condition/SqArea.java
@@ -0,0 +1,13 @@
+package ru.job4j.condition;
+
+public class SqArea {
+    // add method for calculate square
+    // p is perimeter, k is coefficient (length is greater then height)
+    public static double square(int p, int k) {
+        return Math.pow(p / (2 * (k + 1)), 2) * k;
+    }
+    // start program
+    public static void main(String[] args) {
+        System.out.println("Square rectangular is  " + SqArea.square(6, 2));
+    }
+}
