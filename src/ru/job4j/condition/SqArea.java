dac0d986c5b38458efdbfd3f507be58b4767f68b
kva.devops <kva.devops@gmail.com>
2021-03-05 10:54:10 +0400

    стороны прямоугольника SqArea.java - add commentary

--- src/ru/job4j/condition/SqArea.java
+++ src/ru/job4j/condition/SqArea.java
@@ -2,7 +2,7 @@ package ru.job4j.condition;
 
 public class SqArea {
     // add method for calculate square
-    // p is perimeter, k is coefficient (length is greater then height)
+    // p is perimeter, k is coefficient (how mach length is greater then height)
     public static double square(double p, double k) {
         double height = p / (2 * (k + 1));
         double length = height * k;
@@ -12,5 +12,7 @@ public class SqArea {
 
     public static void main(String[] args) {
         System.out.println("Square rectangular is  " + SqArea.square(6, 2));
+        System.out.println("Square rectangular is  " + SqArea.square(10, 3));
+        System.out.println("Square rectangular is  " + SqArea.square(8, 4));
     }
 }
