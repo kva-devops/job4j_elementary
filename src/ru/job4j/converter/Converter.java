0b605875b80ca20e05ff292bac1b09781cde1584
kva.devops <kva.devops@gmail.com>
2021-03-05 08:56:10 +0400

    0. Что такое тестирование [#484909]

--- src/ru/job4j/converter/Converter.java
+++ src/ru/job4j/converter/Converter.java
@@ -5,28 +5,50 @@ public class Converter {
     public static int rubToEur(int value) {
         return value / 70;
     }
-    // method for converting euro to rub
 
+    // method for converting euro to rub
     public static int eurToRub(int value) {
         return value * 70;
     }
-    // method for converting rub to dollar
 
+    // method for converting rub to dollar
     public static int rubToDol(int value) {
         return value / 60;
     }
-    // method for converting dollar to rub
 
+    // method for converting dollar to rub
     public static int dolToRub(int value) {
         return value * 60;
     }
-    // start program
 
+    // start program
     public static void main(String[] args) {
-        int yourCurrency = 100;
-        System.out.println(yourCurrency + " Rub is " + rubToEur(yourCurrency) + " Euro");
-        System.out.println(yourCurrency + " Euro is " + eurToRub(yourCurrency) + " Rub");
-        System.out.println(yourCurrency + " Rub is " + rubToDol(yourCurrency) + " Dollar");
-        System.out.println(yourCurrency + " Dollar is " + dolToRub(yourCurrency) + " Rub");
+        // testing method rubToEur()
+        int in = 140;
+        int expected = 2;
+        int out = Converter.rubToEur(in);
+        boolean passed = expected == out;
+        System.out.println("Testing: rubToEur(). Test result: " + passed);
+
+        // testing method eurToRub()
+        in = 2;
+        expected = 140;
+        out = Converter.eurToRub(in);
+        passed = expected == out;
+        System.out.println("Testing: eurToRub(). Test result: " + passed);
+
+        // testing method rubToDol()
+        in = 120;
+        expected = 2;
+        out = Converter.rubToDol(in);
+        passed = expected == out;
+        System.out.println("Testing: rubToDol(). Test result: " + passed);
+
+        // testing method dolToRub()
+        in = 2;
+        expected = 120;
+        out = Converter.dolToRub(in);
+        passed = expected == out;
+        System.out.println("Testing: dolToRub(). Test result: " + passed);
     }
 }
