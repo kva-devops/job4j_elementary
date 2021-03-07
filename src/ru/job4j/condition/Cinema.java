2e009c9e917ac7811423e88abbdb9f61db269ee6
kva.devops <kva.devops@gmail.com>
2021-03-07 16:55:10 +0400

    4.3.1. Оператор && [#484830]

--- src/ru/job4j/condition/Cinema.java
+++ src/ru/job4j/condition/Cinema.java
@@ -9,9 +9,19 @@ public class Cinema {
             }
     }
 
+    public static void permission(boolean allowByParent, boolean hasMoney) {
+        if (allowByParent && hasMoney) {
+            System.out.println("I can go to the cinema");
+        } else {
+            System.out.println("I can't.");
+        }
+    }
+
     public static void main(String[] args) {
-        Cinema.access(13);
-        Cinema.access(34);
+        permission(true, true);
+        permission(true, false);
+        permission(false, true);
+        permission(false, false);
     }
 
 }
