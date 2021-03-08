f1591107a0bef996ceda5ce11ba260e7acc7f740
kva.devops <kva.devops@gmail.com>
2021-03-08 12:54:28 +0400

    5.2. Создать программу, вычисляющую факториал. [#484943] correcting

--- src/ru/job4j/loop/Factorial.java
+++ src/ru/job4j/loop/Factorial.java
@@ -2,13 +2,9 @@ package ru.job4j.loop;
 
 public class Factorial {
     public static int calc(int n) {
-        int result = 0;
-        for (int i = 0; i <= n; i++) {
-            if (result == 0) {
-                result = ++i;
-            } else {
+        int result = 1;
+        for (int i = 1; i <= n; i++) {
                 result *= i;
-            }
         }
         return result;
     }
