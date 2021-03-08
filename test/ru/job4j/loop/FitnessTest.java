c8113301fabe23f25e74aaa76984f11c444b4879
kva.devops <kva.devops@gmail.com>
2021-03-08 13:20:17 +0400

    5.4. Протеиновая диета [#484934]

---
+++ test/ru/job4j/loop/FitnessTest.java
@@ -0,0 +1,28 @@
+package ru.job4j.loop;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class FitnessTest {
+
+    @Test
+    public void whenIvanGreatNik() {
+        int out = Fitness.calc(95, 90);
+        int expected = 0;
+        Assert.assertEquals(expected, out);
+    }
+
+    @Test
+    public void whenIvanLessNik() {
+        int out = Fitness.calc(90, 95);
+        int expected = 1;
+        Assert.assertEquals(expected, out);
+    }
+
+    @Test
+    public void whenIvanLessFewNik() {
+        int out = Fitness.calc(50, 90);
+        int expected = 2;
+        Assert.assertEquals(expected, out);
+    }
+}
