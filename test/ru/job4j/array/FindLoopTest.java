b682c4965bea97f00e90cf34c8efe4bf54ef05e2
kva.devops <kva.devops@gmail.com>
2021-03-10 09:07:10 +0400

    6.5. Поиск индекса в диапазоне. [#484851]

--- test/ru/job4j/array/FindLoopTest.java
+++ test/ru/job4j/array/FindLoopTest.java
@@ -14,4 +14,15 @@ public class FindLoopTest {
         int expected = 0;
         Assert.assertThat(out, is(expected));
     }
+
+    @Test
+    public void whenFind3() {
+        int[] input = new int[] {5, 2, 10, 2, 4};
+        int value = 2;
+        int start = 2;
+        int finish = 4;
+        int result = FindLoop.indexOf(input, value, start, finish);
+        int expect = 3;
+        Assert.assertThat(result, is(expect));
+    }
 }
