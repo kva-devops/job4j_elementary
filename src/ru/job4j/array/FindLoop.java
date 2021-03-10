9b792c797665737dbb730ee4b8e62dae011d1053
kva.devops <kva.devops@gmail.com>
2021-03-10 10:52:52 +0400

    6.5. Поиск индекса в диапазоне. [#484851] correcting

--- src/ru/job4j/array/FindLoop.java
+++ src/ru/job4j/array/FindLoop.java
@@ -14,7 +14,7 @@ public class FindLoop {
 
     public static int indexOf(int[] data, int element, int start, int finish) {
         int result = -1;
-        for (int i = start; i < finish; i++) {
+        for (int i = start; i <= finish; i++) {
             if (data[i] == element) {
                 result = i;
                 break;
